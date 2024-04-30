331ec8c08e2118f9a60e28049e9d11dba069e861
Pamela <pamela_borges18@hotmail.com>
2024-04-30 16:34:18 -0300

    Iniciando o projeto e criando o dominio

---
+++ dev-week/src/main/java/me/dio/devweek/DevWeekApplication.java
@@ -0,0 +1,13 @@
+package me.dio.devweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevWeekApplication.class, args);
+	}
+
+}
